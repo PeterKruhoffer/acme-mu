bf48d14bd2b478f7f69edfa5cce3289352fee362
peterkruhoffer <peter.kruhoffer@gmail.com>
2023-10-19 12:48:32 +0200

    Chaning text for info card in index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -32,7 +32,11 @@ export default function TabOneScreen() {
       <UserInfo />
       <View className="py-8" />
       <View className="px-4">
-        <InfoCard item={{ title: "Hvis du ændrer til/fra Danmark, så kontakt os på 33 95 97 00" }} />
+        <InfoCard
+          item={{
+            title: "Hvis du ændrer til/fra Danmark, så kontakt os på 33 95 97 00"
+          }}
+        />
       </View>
       <Text className="text-2xl px-4 py-8">Aktuelt</Text>
       <FlashList
