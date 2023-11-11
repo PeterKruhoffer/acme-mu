2e3e8c4c70ae9e3ff03972b4efb39fd7f43f63de
peterkruhoffer <peter.kruhoffer@gmail.com>
2023-11-11 17:34:31 +0100

    Starting mentor styling

--- app/(tabs)/mentor.tsx
+++ app/(tabs)/mentor.tsx
@@ -1,11 +1,93 @@
-import { View, Text } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import { View, Text, useWindowDimensions } from "react-native";
+
+const DATA = [
+  {
+    id: "1",
+    type: "Mentor",
+    name: "Navn",
+    area: "Område",
+    occupation: "Beskæftigelse",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "2",
+    type: "Mentor",
+    name: "Navn",
+    area: "Område",
+    occupation: "Beskæftigelse",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "3",
+    type: "Mentor",
+    name: "Navn",
+    area: "Område",
+    occupation: "Beskæftigelse",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "4",
+    type: "Mentor",
+    name: "Navn",
+    area: "Område",
+    occupation: "Beskæftigelse",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "5",
+    type: "Mentor",
+    name: "Navn",
+    area: "Område",
+    occupation: "Beskæftigelse",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+]
 
 function Mentor() {
+  const { width } = useWindowDimensions();
+  const cardWidth = 0.85 * width;
+  const seperatorWidth = 4;
+  const snapToIntervalValue = cardWidth + seperatorWidth;
   return (
-    <View className="flex justify-center items-center py-20">
-      <Text className="text-2xl">Mentor/Mentee</Text>
-    </View>
+    <>
+      <View className="flex py-20 px-4">
+        <Text className="text-2xl">Mentor/Mentee</Text>
+      </View>
+      <FlashList
+        data={DATA}
+        estimatedItemSize={300}
+        decelerationRate="fast"
+        snapToInterval={snapToIntervalValue}
+        snapToAlignment="start"
+        disableAutoLayout
+        horizontal
+        ItemSeparatorComponent={() => <View className="p-1" />}
+        contentContainerStyle={{ paddingHorizontal: 16 }}
+        renderItem={({ item }) => <MentorCard item={item} />}
+      />
+    </>
   );
 }
 
 export default Mentor;
+
+function MentorCard(props: { item: (typeof DATA)[0] }) {
+  return (
+    <View className="bg-blue-600 w-[85vw] h-full p-4">
+      <View className="flex flex-col gap-2 relative">
+        <Text className="text-xl font-bold text-white">{props.item.name}</Text>
+        <Text className="text-lg text-white">{props.item.area}</Text>
+        <Text className="text-lg text-white">{props.item.occupation}</Text>
+        <View className="rounded-full bg-black h-24 w-24 absolute top-0 right-2 flex justify-center items-center">
+          <Text className="text-white text-sm">IMG</Text>
+        </View>
+      </View>
+      <View className="w-fit py-4">
+        <Text className="text-lg text-white" numberOfLines={10}>
+          {props.item.description}
+        </Text>
+      </View>
+    </View>
+  );
+}
